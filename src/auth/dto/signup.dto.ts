782b18513653b9eb4ac06bfb0e740d5807303612
DarshilChauhan1 <chauhandarshil716@gmail.com>
2024-04-04 16:00:17 +0530

    changes

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,15 +1,19 @@
-import { IsEmail, IsString } from "class-validator";
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class SignupDto {
+    @IsNotEmpty()
     @IsString()
     name: string;
 
+    @IsNotEmpty()
     @IsString()
     username: string;
 
+    @IsNotEmpty()
     @IsEmail()
     email: string;
 
+    @IsNotEmpty()
     @IsString()
     password: string;
 }
